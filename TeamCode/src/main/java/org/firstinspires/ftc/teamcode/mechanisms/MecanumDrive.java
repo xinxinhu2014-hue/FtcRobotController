208752866a26588be5fb193450970d623b9b31c1
Hudson <hudson_meng@s.thevillageschool.com>
2025-11-05 02:06:27 -0600

    MecanumDrive: add goStraight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MecanumDrive.java
@@ -5,9 +5,8 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class MecanumDrive {
     private static final double TICK_PER_REV = 28 * 19.2;
-    private static final double WHEEL_CIRCUM = 4 * 3.14;
+    private static final double WHEEL_CIRCUM = 4 * Math.PI;
     private static final double GEAR_RATIO = 60.0/40.0;
-
     private final DcMotor[] wheelsMotor = new DcMotor[4];
 
 
@@ -23,7 +22,10 @@ public class MecanumDrive {
         wheelsMotor[3].setDirection(DcMotor.Direction.FORWARD);
 
         for (DcMotor motor : wheelsMotor) {
+            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            motor.setPower(0);
         }
     }
 
@@ -38,19 +40,38 @@ public class MecanumDrive {
         }
     }
 
-    public void drive(double forward, double right, double rotate) {
+    public void drive(double forward, double side, double rotate) {
         double[] wheelsPower = new double[4];
-        wheelsPower[0] = forward + right + rotate;
-        wheelsPower[1] = forward - right - rotate;
-        wheelsPower[2] = forward - right + rotate;
-        wheelsPower[3] = forward + right - rotate;
+        wheelsPower[0] = forward + side + rotate;
+        wheelsPower[1] = forward - side - rotate;
+        wheelsPower[2] = forward - side + rotate;
+        wheelsPower[3] = forward + side - rotate;
 
         setPower(wheelsPower);
     }
 
-
     public void goStraight(double distance, double power) {
-        double targetTick = distance / WHEEL_CIRCUM / GEAR_RATIO * TICK_PER_REV;
+        int targetTick = (int) (distance / WHEEL_CIRCUM / GEAR_RATIO * TICK_PER_REV);
+        int dirSign;
+        for (int i = 0; i < 4; i++) {
+            dirSign = (wheelsMotor[i].getDirection() == DcMotor.Direction.REVERSE) ? -1 : 1;
+            wheelsMotor[i].setTargetPosition(targetTick * dirSign);
+            wheelsMotor[i].setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+        for (int i = 0; i < 4; i++) {
+            wheelsMotor[i].setPower(power);
+        }
+
+        while (wheelsMotor[0].isBusy() || wheelsMotor[1].isBusy() || wheelsMotor[2].isBusy() || wheelsMotor[3].isBusy()) {
+            // optional: add telemetry here
+        }
+
+        // Stop and hold
+        for (DcMotor motor : wheelsMotor) {
+            motor.setPower(0);
+            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
 
     }
 }

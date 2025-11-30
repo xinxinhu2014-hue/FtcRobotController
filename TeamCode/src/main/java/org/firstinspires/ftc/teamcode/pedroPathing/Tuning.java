044febf05b5579fd39e43e2844eb36c7b0492cfd
Hudson <hudson_meng@s.thevillageschool.com>
2025-11-30 15:51:25 -0600

    PedroPathing Tune at ForwardZeroPowerAccelerationTuner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
@@ -536,7 +536,7 @@ class LateralVelocityTuner extends OpMode {
  */
 class ForwardZeroPowerAccelerationTuner extends OpMode {
     private final ArrayList<Double> accelerations = new ArrayList<>();
-    public static double VELOCITY = 30;
+    public static double VELOCITY = 1;
 
     private double previousVelocity;
     private long previousTimeNano;
